bbb7f2ea806c9c4827436872106f6482c88932e3
Ed Cole <edcole@mail.sunyjcc.edu>
2016-09-02 15:42:54 -0400

    Upgrade jquery-ui to 1.12.0

--- JqueryUiAssetsGrailsPlugin.groovy
+++ JqueryUiAssetsGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class JqueryUiAssetsGrailsPlugin {
     // the plugin version
-    def version = "1.11.1"
+    def version = "1.12.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
